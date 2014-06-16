a1745e0d5b08bc065ed595734e2af7b7a8078819
Ankur Goel <ankurgel@gmail.com>
2014-06-16 20:21:56 +0530

    added basic user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ActiveRecord::Base
+  before_save { email.downcase! }
+  has_secure_password
+
+  # Validations
+  validates :name, presence: true,
+                   length: { maximum: 60 }
+  validates :email, presence: true,
+                    uniqueness: { case_sensitive: false },
+                    format: { with: /\A[\w+.]+@[\w\-.]+\.[a-z]+\z/i }
+
+  validates :password, length: { minimum: 6 }
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140616142035) do
+ActiveRecord::Schema.define(version: 20140616144319) do
 
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "password_digest"
     t.string   "remember_token"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
