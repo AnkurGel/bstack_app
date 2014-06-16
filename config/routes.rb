53a954489c2e275a370918bfeca738370899b618
Ankur Goel <ankurgel@gmail.com>
2014-06-16 21:02:07 +0530

    added signup form for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Bstack::Application.routes.draw do
+  resources :users
+
   root 'default_pages#home'
+  get '/signup' => 'users#new'
+  #get '/login' => 'sessions#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
