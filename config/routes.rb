90683359c6fb20d827063f9869c7a4440330b941
Ankur Goel <ankurgel@gmail.com>
2014-06-18 02:18:02 +0530

    Rolling out authentication with Devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Bstack::Application.routes.draw do
+  devise_for :users, path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registrations: 'signup'
+  }
+
+  devise_scope :user do
+    delete '/logout' => 'devise/sessions#destroy', as: :logout
+    get '/login' => 'devise/sessions#new', as: :login
+    get '/signup' => 'devise/registrations#new', as: :signup
+  end
+
   root 'default_pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
