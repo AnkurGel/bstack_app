58e757c6e948010acaa1b1e3d96de66e57de0ae9
Ankur Goel <ankurgel@gmail.com>
2014-06-17 13:17:00 +0530

    added authorization for correct user while uploading or accessing a file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,18 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  def signed_in_user
+    unless signed_in?
+      flash[:error] = "You need to sign in first, to upload a file"
+      redirect_to root_url 
+    end
+  end
+
+  def authorized_user
+    if current_user != Upload.find(params[:id]).user
+      flash[:error] = "You are not authorized to have any access for this file"
+      redirect_to root_url
+    end
+  end
 end
